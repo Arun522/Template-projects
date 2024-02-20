3b571b121e53447704ec62ed3b92a5f6f33801f7
Arunkumar velu <mailarun522@gmail.com>
2024-02-20 10:52:12 +0530

    Added project folders

---
+++ Weather-app/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "65172b06bfa43baac8d98719c7f9c95e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
